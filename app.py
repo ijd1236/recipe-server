3173ff44c8ba24cd256917dc3a2978288b863e99
ijd1236 <kjy960219@gmail.com>
2023-06-16 18:08:10 +0900

    a

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
-
 from flask import Flask
-
+from flask_restful import Api
+from resources.recipe import RecipeListResource, RecipeResource
 
 app = Flask(__name__)
 
+api = Api(app) # Flask 객체에 Api 객체 등록
+
+# 경로와 API 동작코드(Resourece)를 연결한다.
+api.add_resource( RecipeListResource , '/recipes')
+api.add_resource(RecipeResource, '/recipes/<int:recipe_id>')
+
 if __name__ == '__main__':
     app.run()
+
+
+
+
+
