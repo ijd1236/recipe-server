bd6868e9730f9886b18d1d89ba08a3d1a92a9c76
ijd1236 <kjy960219@gmail.com>
2023-06-19 10:33:50 +0900

    a

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ api = Api(app) # Flask 객체에 Api 객체 등록
 # 경로와 API 동작코드(Resourece)를 연결한다.
 api.add_resource( RecipeListResource , '/recipes')
 api.add_resource(RecipeResource, '/recipes/<int:recipe_id>')
+api.add_resource( UserRegisterResource , '/ruser/register')
 
 if __name__ == '__main__':
     app.run()
---
+++ resources/user.py
@@ -0,0 +1,35 @@
+from flask_restful import Resource
+from flask import request
+from mysql_connection import get_connection
+from mysql.connector import Error
+import mysql.connector
+from email_validator import validate_email, EmailNotValidError     
+
+# EmailNotValidError 이메일이 정상이냐
+
+
+# 클래스는 변수와 함수의 묶음
+
+class UserRegisterResource(Resource):
+    def post(self) :
+        # {"username" : "홍길동",
+        # "email" : "abc@naver.com",
+        # "password" : "1234"}
+        
+        # 1. 클라이언트가 보낸 데이터를 받아준다.
+
+        data = request.get_json()
+
+        # 2. 이메일 주소형식이 올바른지 확인한다.
+        try :
+            validate_email(data['email'])
+        
+        
+
+
+        except EmailNotValidError as e :
+            return {'result' : 'fail', 'error' : str(e)} , 
+
+
+
+        return
